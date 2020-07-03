5ace84c9224dc8a95c4d942e20f0f6b9c158897e
Jonathan <jonathan.xer@gmail.com>
2020-07-03 15:15:28 +0200

    Cositis

--- PantallaInicial.py
+++ PantallaInicial.py
@@ -14,17 +14,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_mainWindow(object):
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("mainWindow")
-        mainWindow.resize(690, 745)
+        mainWindow.resize(728, 822)
         self.centralwidget = QtWidgets.QWidget(mainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(210, 40, 271, 61))
-        self.label.setTextFormat(QtCore.Qt.RichText)
-        self.label.setScaledContents(True)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
         self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget.setGeometry(QtCore.QRect(130, 110, 471, 71))
+        self.formLayoutWidget.setGeometry(QtCore.QRect(130, 240, 471, 71))
         self.formLayoutWidget.setObjectName("formLayoutWidget")
         self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
         self.formLayout.setContentsMargins(0, 0, 0, 0)
@@ -44,17 +38,20 @@ class Ui_mainWindow(object):
         self.passwordLineEdit.setObjectName("passwordLineEdit")
         self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.passwordLineEdit)
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(270, 190, 75, 23))
+        self.pushButton.setGeometry(QtCore.QRect(280, 320, 75, 23))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(360, 190, 75, 23))
+        self.pushButton_2.setGeometry(QtCore.QRect(370, 320, 75, 23))
         self.pushButton_2.setObjectName("pushButton_2")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(100, 230, 500, 375))
+        self.label_3.setGeometry(QtCore.QRect(120, 370, 501, 361))
         self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(260, 30, 201, 201))
+        self.label_4.setObjectName("label_4")
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(mainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 690, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 728, 21))
         self.menubar.setObjectName("menubar")
         mainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(mainWindow)
@@ -67,13 +64,13 @@ class Ui_mainWindow(object):
     def retranslateUi(self, mainWindow):
         _translate = QtCore.QCoreApplication.translate
         mainWindow.setWindowTitle(_translate("mainWindow", "MainWindow"))
-        self.label.setText(_translate("mainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:26pt; font-style:italic; color:#26e2ff;\">PISCIGO</span></p></body></html>"))
         self.label_2.setText(_translate("mainWindow", "Usuario:"))
         self.passwordLabel.setText(_translate("mainWindow", "Password:"))
         self.pushButton.setText(_translate("mainWindow", "Log In"))
         self.pushButton_2.setText(_translate("mainWindow", "Register"))
-        self.label_3.setText(_translate("mainWindow", "<html><head/><body><p><img src=\":/Test/C:/Users/Mi Pc/Downloads/piscinacomunitaria.jpg\"/></p></body></html>"))
-import test_rc
+        self.label_3.setText(_translate("mainWindow", "<html><head/><body><p><img src=\":/Piscina/piscinacomunitaria.jpg\"/></p></body></html>"))
+        self.label_4.setText(_translate("mainWindow", "<html><head/><body><p><img src=\":/Logo/Logo.png\" width=\"200\" height=\"200\"/></p></body></html>"))
+import Resources_rc
 
 
 if __name__ == "__main__":
