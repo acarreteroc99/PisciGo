84d794a0b3d00d3e1a72e7b5e83f3f1d149429fa
Jonathan <jonathan.xer@gmail.com>
2020-07-03 16:24:01 +0200

    Button working

--- Logica.py
+++ Logica.py
@@ -1,16 +1,26 @@
 from PantallaInicial import *
+from RegistroUsuario import *
 
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class RegisterWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
-    def __init__(self, *args, **kwargs):
-        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
-        self.setupUi(self)
+	def __init__(self, *args, **kwargs):
+		QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+		self.setupUi(self)
 
 
+class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
+	def __init__(self, *args, **kwargs):
+		QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+		self.setupUi(self)
+		self.registerWindow=RegisterWindow()
+		self.pushButton_2.clicked.connect(self.showRegister)
+
+	def showRegister(self):
+		self.registerWindow.show()
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec_()
+	app = QtWidgets.QApplication([])
+	window = MainWindow()
+	window.show()
+	app.exec_()
